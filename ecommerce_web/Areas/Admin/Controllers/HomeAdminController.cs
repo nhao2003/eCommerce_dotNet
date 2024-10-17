4992e40024ff36df263ddea7dcf14ce424183f38
Nguyen Nhat Hao <nhathaodev@gmail.com>
2024-10-17 14:45:00 +0000

    Build product category page for Admin

--- ecommerce_web/Areas/Admin/Controllers/HomeAdminController.cs
+++ ecommerce_web/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ecommerce_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
 
 namespace ecommerce_web.Areas.Admin.Controllers
 {
@@ -7,11 +10,22 @@ namespace ecommerce_web.Areas.Admin.Controllers
     [Route("admin/homeadmin")]
     public class HomeAdminController : Controller
     {
+        QlBanValiContext db = new QlBanValiContext();
         [Route("")]
         [Route("index")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
+            return View(lst);
         }
     }
 }
